82aa333e7cc08b411d8e9c594ad7f2e93004983d
burhaneyuboglu <burhaneyuboglu@gmail.com>
2021-07-25 13:43:25 +0100

    a

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
+      title: "New Desk (Woodene)",
       amount: 450,
       date: new Date(2021, 5, 12),
     },
